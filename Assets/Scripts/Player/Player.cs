43ac73c802d02c347c293edd6c5f2cde63c925e3
Alexander <madhamst3r@gmail.com>
2021-11-20 02:47:16 +0300

    Try implement PlayerControl.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,7 +1,11 @@
 
+using TMPro.EditorUtilities;
+using UnityEditor;
 using UnityEngine;
 [RequireComponent(typeof(PlayerControl))]
 public class Player : MonoBehaviour
 {
-   
+    [SerializeField] private float _speed;
+    public float Speed => _speed;
+    
 }
--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,10 +1,53 @@
 
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof( Player))]
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] private Player _player;
+    private Coroutine _moveRoutine = null;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // var ray = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+           // Debug.Log(ray);
+
+            Move(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            // Move(Camera.main.ViewportToWorldPoint(Input.mousePosition));
+            // Move(ray);
+            // Debug.Log(Input.mousePosition.normalized);
+        }
+    }
+
+    private void Move(Vector3 position)
+    {
+        Debug.Log(position);
+        if (_moveRoutine != null)
+        {
+            StopCoroutine(_moveRoutine);
+        }
+            
+        _moveRoutine = StartCoroutine(ChangePlayerPositionRoutine(position));
+    }
+    private IEnumerator ChangePlayerPositionRoutine(Vector3 target)
+    {
+        var currentPosition = _player.transform.position;
+        var targetPosition = new Vector3(target.x, currentPosition.y, currentPosition.z);
+        
+        while (_player.transform.position != targetPosition)
+        {
+            _player.transform.position =
+                Vector3.MoveTowards(_player.transform.position, targetPosition, _player.Speed * Time.deltaTime);
+            yield return null;
+           
+            
+        }
+
+    }
     
 }
