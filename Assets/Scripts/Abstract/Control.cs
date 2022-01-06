79c8ca1d8f5e497409a467c03b32e04a6d6548e8
Alexander <madhamst3r@gmail.com>
2022-01-07 00:39:39 +0300

    bugfix

--- Assets/Scripts/Abstract/Control.cs
+++ Assets/Scripts/Abstract/Control.cs
@@ -49,7 +49,9 @@ using UnityEngine;
             while (Math.Abs(_player.transform.position.x - target.x) > 0.1f)
             {
                 _player.transform.position =
-                    Vector3.MoveTowards(_player.transform.position, target, _player.Sensitivity * Time.deltaTime);
+                    Vector3.Lerp(_player.transform.position, target, _player.Sensitivity * Time.deltaTime);
+                currentPosition = _player.transform.position;
+                target.z = currentPosition.z;
                 
                 yield return null;
             
